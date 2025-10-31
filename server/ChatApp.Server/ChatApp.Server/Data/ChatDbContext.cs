cc06713351b082f798378dc70fe2e0a30d90736d
Vanya Lutsenko <vanyalucenko3@gmail.com>
2025-10-31 18:48:46 +0200

    Add Message entity and initial EF Core migration

--- server/ChatApp.Server/ChatApp.Server/Data/ChatDbContext.cs
+++ server/ChatApp.Server/ChatApp.Server/Data/ChatDbContext.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ChatApp.Server.Models;
 
 namespace ChatApp.Server.Data
 {
     public class ChatDbContext : DbContext
     {
+        public ChatDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Message> Messages { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Message>();
+        }
     }
 }
--- server/ChatApp.Server/ChatApp.Server/Models/Message.cs
+++ server/ChatApp.Server/ChatApp.Server/Models/Message.cs
@@ -2,5 +2,9 @@
 {
     public class Message
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime TimeSent { get; set; }
+        public string UserSent { get; set; }
     }
 }
