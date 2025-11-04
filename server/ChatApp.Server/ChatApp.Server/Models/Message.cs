fbb33a866ce8285f3151b1453030a2cd8552a6eb
Vanya Lutsenko <vanyalucenko3@gmail.com>
2025-11-04 03:15:20 +0200

    Set Message.Id to auto-increment identity column

--- server/ChatApp.Server/ChatApp.Server/Models/Message.cs
+++ server/ChatApp.Server/ChatApp.Server/Models/Message.cs
@@ -1,7 +1,10 @@
-﻿namespace ChatApp.Server.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChatApp.Server.Models
 {
     public class Message
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Text { get; set; }
         public DateTime TimeSent { get; set; }
